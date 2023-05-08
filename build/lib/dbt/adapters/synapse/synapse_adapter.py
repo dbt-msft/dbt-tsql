e4e777235ab373d367002949b8e4b57400517923
Anders Swanson <anders.swanson@dbtlabs.com>
2023-05-08 13:58:10 -0400

    drop __pycache__ files
    Delete test_connections.cpython-38-pytest-7.1.1.pyc
    
    Delete dbt/adapters/__pycache__ directory
    
    Delete dbt/adapters/sqlserver/__pycache__ directory
    
    Delete dbt/adapters/synapse/__pycache__ directory
    
    Delete tests/__pycache__ directory
    
    Delete tests/unit/adapters/sqlserver/__pycache__ directory
    
    Delete dbt/include/__pycache__ directory
    
    Delete dbt/include/sqlserver/__pycache__ directory

--- build/lib/dbt/adapters/synapse/synapse_adapter.py
+++
@@ -1,7 +0,0 @@
-from dbt.adapters.sqlserver import SQLServerAdapter
-
-from dbt.adapters.synapse.synapse_connection_manager import SynapseConnectionManager
-
-
-class SynapseAdapter(SQLServerAdapter):
-    ConnectionManager = SynapseConnectionManager
--- build/lib/dbt/adapters/synapse/synapse_connection_manager.py
+++
@@ -1,6 +0,0 @@
-from dbt.adapters.sqlserver import SQLServerConnectionManager
-
-
-class SynapseConnectionManager(SQLServerConnectionManager):
-    TYPE = "synapse"
-    TOKEN = None
